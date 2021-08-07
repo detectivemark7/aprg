2b0f17c2c2a4d27d1d071787a71f47cef7c3bb4e
detectivemark7 <detectivemark7@gmail.com>
2021-08-08 02:00:02 +0800

    update

--- AprgAudio/AprgAudio/tst/AudioTests/Audio_unit.cpp
+++ AprgAudio/AprgAudio/tst/AudioTests/Audio_unit.cpp
@@ -13,7 +13,8 @@
 #include <AudioTests/ExpectedValues/wav_stereo_24bit_48000.hpp>
 #include <AudioTests/ExpectedValues/wav_stereo_8bit_44100.hpp>
 #include <AudioTests/ExpectedValues/wav_stereo_8bit_48000.hpp>
-#include <Common/Math/AlbaMathHelper.hpp>
+#include <Common/Math/Helpers/ConstantHelpers.hpp>
+#include <Common/Math/Helpers/PrecisionHelpers.hpp>
 
 #include <gtest/gtest.h>
 
