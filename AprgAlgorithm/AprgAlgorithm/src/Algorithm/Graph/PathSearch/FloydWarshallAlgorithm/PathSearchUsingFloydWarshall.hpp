9e90e46b61fb51c2ce3f66208903b177b0077b09
detectivemark7 <detectivemark7@gmail.com>
2021-08-11 13:24:38 +0800

    update

--- AprgAlgorithm/AprgAlgorithm/src/Algorithm/Graph/PathSearch/FloydWarshallAlgorithm/PathSearchUsingFloydWarshall.hpp
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Graph/PathSearch/FloydWarshallAlgorithm/PathSearchUsingFloydWarshall.hpp
@@ -207,6 +207,10 @@ std::ostream & operator<<(std::ostream & out, typename PathSearchUsingFloydWarsh
     return out;
 }
 
+}
+
+}
+
 
 // Algorithm in short terms: Use a "adjacent/best weight" matrix to find shortest/longest by checking vertices in between vertices.
 
@@ -223,6 +227,61 @@ std::ostream & operator<<(std::ostream & out, typename PathSearchUsingFloydWarsh
 // However, the algorithm can only be used when the graph is so small that a cubic time complexity is fast enough.
 // And the matrix type structure makes it easier to have "start" to "end" path queries
 
-}
+// Other discussions
+// The Floyd Warshall Algorithm is for solving the All Pairs Shortest Path problem.
+// The problem is to find shortest distances between every pair of vertices in a given edge weighted directed Graph.
+// Example:
+//
+// Input:
+//        graph[][] = { {0,   5,  INF, 10},
+//                     {INF,  0,  3,  INF},
+//                     {INF, INF, 0,   1},
+//                     {INF, INF, INF, 0} }
+// which represents the following graph
+//              10
+//        (0)------->(3)
+//         |         /|\
+//       5 |          |
+//         |          | 1
+//        \|/         |
+//        (1)------->(2)
+//             3
+// Note that the value of graph[i][j] is 0 if i is equal to j
+// And graph[i][j] is INF (infinite) if there is no edge from vertex i to j.
+//
+// Output:
+// Shortest distance matrix
+//       0      5      8      9
+//     INF      0      3      4
+//     INF    INF      0      1
+//     INF    INF    INF      0
 
-}
+// Floyd Warshall Algorithm
+// We initialize the solution matrix same as the input graph matrix as a first step.
+// Then we update the solution matrix by considering all vertices as an intermediate vertex.
+// The idea is to one by one pick all vertices and updates all shortest paths which include the picked vertex
+// as an intermediate vertex in the shortest path.
+// When we pick vertex number k as an intermediate vertex,
+// we already have considered vertices {0, 1, 2, .. k-1} as intermediate vertices.
+// For every pair (i, j) of the source and destination vertices respectively, there are two possible cases.
+// -> 1) k is not an intermediate vertex in shortest path from i to j.
+// ---> We keep the value of dist[i][j] as it is.
+// -> 2) k is an intermediate vertex in shortest path from i to j.
+// ---> We update the value of dist[i][j] as dist[i][k] + dist[k][j] if dist[i][j] > dist[i][k] + dist[k][j]
+
+// Time Complexity: O(V^3)
+// The above program only prints the shortest distances.
+// We can modify the solution to print the shortest paths also by storing the predecessor information in a separate 2D matrix.
+// Also, the value of INF can be taken as INT_MAX from limits.h to make sure that we handle maximum possible value.
+// When we take INF as INT_MAX, we need to change the if condition in the above program to avoid arithmetic overflow.
+// #include
+//
+// #define INF INT_MAX
+// ..........................
+// if ( dist[i][k] != INF &&
+//      dist[k][j] != INF &&
+//      dist[i][k] + dist[k][j] < dist[i][j]
+//     )
+//  dist[i][j] = dist[i][k] + dist[k][j];
+// ...........................
+
