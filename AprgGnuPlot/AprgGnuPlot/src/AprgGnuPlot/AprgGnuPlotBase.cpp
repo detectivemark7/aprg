750f99e6e404c26cb424e07ccc1b2c3d2cf398ac
detectivemark7 <detectivemark7@gmail.com>
2021-09-20 00:25:25 +0800

    update

--- AprgGnuPlot/AprgGnuPlot/src/AprgGnuPlot/AprgGnuPlotBase.cpp
+++ AprgGnuPlot/AprgGnuPlot/src/AprgGnuPlot/AprgGnuPlotBase.cpp
@@ -7,25 +7,25 @@ namespace alba
 
 void AprgGnuPlotBase::setTitle(string const& title, string const& configurationString)
 {
-    m_gnuPlot << R"(set title ")" << title << " " << configurationString << endl;
+    m_gnuPlot << R"(set title ")" << title << " " << configurationString << "\n";
 }
 
 void AprgGnuPlotBase::setXAxis(string const& xAxisName, string const& configurationString)
 {
-    m_gnuPlot << R"(set xlabel ")" << xAxisName << " " << configurationString << endl;
-    m_gnuPlot << R"(set xtics font ",15")" << endl;
+    m_gnuPlot << R"(set xlabel ")" << xAxisName << " " << configurationString << "\n";
+    m_gnuPlot << R"(set xtics font ",15")" << "\n";
 }
 
 void AprgGnuPlotBase::setYAxis(string const& yAxisName, string const& configurationString)
 {
-    m_gnuPlot << R"(set ylabel ")" << yAxisName << " " << configurationString << endl;
-    m_gnuPlot << R"(set ytics font ",15")" << endl;
+    m_gnuPlot << R"(set ylabel ")" << yAxisName << " " << configurationString << "\n";
+    m_gnuPlot << R"(set ytics font ",15")" << "\n";
 }
 
 void AprgGnuPlotBase::setZAxis(string const& zAxisName, string const& configurationString)
 {
-    m_gnuPlot << R"(set zlabel ")" << zAxisName << " " << configurationString << endl;
-    m_gnuPlot << R"(set ztics font ",15")" << endl;
+    m_gnuPlot << R"(set zlabel ")" << zAxisName << " " << configurationString << "\n";
+    m_gnuPlot << R"(set ztics font ",15")" << "\n";
 }
 
 void AprgGnuPlotBase::doGraphs(GraphingSteps const& graphingSteps)
@@ -37,7 +37,7 @@ void AprgGnuPlotBase::doGraphs(GraphingSteps const& graphingSteps)
 
 void AprgGnuPlotBase::endGraph()
 {
-    m_gnuPlot << endl;
+    m_gnuPlot << "\n";
 }
 
 }
