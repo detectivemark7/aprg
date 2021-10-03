e7b4ff7d609c25ae72cb69ff6fc7c210cac8fbbe
detectivemark7 <detectivemark7@gmail.com>
2021-10-03 09:30:21 +0800

    update

--- AprgAlgorithm/AprgAlgorithm/src/Algorithm/Analysis/RequiredTimeComplexity.hpp
+++ AprgAlgorithm/AprgAlgorithm/src/Algorithm/Analysis/RequiredTimeComplexity.hpp
@@ -18,7 +18,7 @@ namespace algorithm
 // | n<=20      | O(2^n)
 // | n<=500     | O(n^3)
 // | n<=5000    | O(n^2)
-// | n<=106     | O(nlogn) or O(n)
+// | n<=10^6    | O(nlogn) or O(n)
 // | n is large | O(1) or O(logn)
 
 
