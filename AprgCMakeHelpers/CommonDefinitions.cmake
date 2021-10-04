015844907be7133fcbaed4cf2e957c4268546fc6
detectivemark7 <detectivemark7@gmail.com>
2021-10-05 06:16:46 +0800

    update

--- AprgCMakeHelpers/CommonDefinitions.cmake
+++ AprgCMakeHelpers/CommonDefinitions.cmake
@@ -20,12 +20,30 @@ elseif(UNIX)
     set(COMMON_DEFINITIONS ${COMMON_DEFINITIONS} -DOS_LINUX)
 endif()
 
-find_package(Subversion)
-if(SUBVERSION_FOUND)
-    Subversion_WC_INFO(${PROJECT_SOURCE_DIR} SvnProject IGNORE_SVN_FAILURE)
-    message("Code revision is ${SvnProject_WC_REVISION}")
-    set(COMMON_DEFINITIONS ${COMMON_DEFINITIONS} -DAPRG_CODE_REVISION="${SvnProject_WC_REVISION}")
+#Use Git to get code version
+# Use [git rev-parse HEAD] to get full git version
+# Use [git rev-parse --short HEAD] to get short version
+find_package(Git) # Requires CMake 2.8.11+
+if(GIT_EXECUTABLE)
+    execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        RESULT_VARIABLE status
+        OUTPUT_VARIABLE GIT_CODE_VERSION
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(status)
+        set(GIT_CODE_VERSION "NoVersion")
+    endif()
 endif()
+set(COMMON_DEFINITIONS ${COMMON_DEFINITIONS} -DAPRG_CODE_REVISION="${GIT_CODE_VERSION}")
+
+#SVN is not used anymore
+#find_package(Subversion)
+#if(SUBVERSION_FOUND)
+#    Subversion_WC_INFO(${PROJECT_SOURCE_DIR} SvnProject IGNORE_SVN_FAILURE)
+#    message("Code revision is ${SvnProject_WC_REVISION}")
+#    set(COMMON_DEFINITIONS ${COMMON_DEFINITIONS} -DAPRG_CODE_REVISION="${SvnProject_WC_REVISION}")
+#endif()
 
 if (TEST_MODE)
     set(COMMON_DEFINITIONS ${COMMON_DEFINITIONS} -DAPRG_TEST_MODE_ON)
