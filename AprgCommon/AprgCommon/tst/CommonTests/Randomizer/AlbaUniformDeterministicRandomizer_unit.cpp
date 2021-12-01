35666c550074ed2c87e2f6e26f08ac001b63fabf
detectivemark7 <detectivemark7@gmail.com>
2021-12-01 13:26:32 +0800

    update

---
+++ AprgCommon/AprgCommon/tst/CommonTests/Randomizer/AlbaUniformDeterministicRandomizer_unit.cpp
@@ -0,0 +1,109 @@
+#include <Common/Randomizer/AlbaUniformDeterministicRandomizer.hpp>
+
+#include <gtest/gtest.h>
+
+#include <vector>
+
+using namespace std;
+
+namespace alba {
+
+namespace {
+using IntegerRandomizerForTest = AlbaUniformDeterministicRandomizer<int>;
+using FloatingPointRandomizerForTest = AlbaUniformDeterministicRandomizer<double>;
+}  // namespace
+
+TEST(AlbaUniformDeterministicRandomizerTest, GetRandomIntegerWorks_WithinMinimumAndMaximumValues) {
+    constexpr int minimumValue(0);
+    constexpr int maximumValue(9);
+    constexpr int customSeed(5);
+    IntegerRandomizerForTest randomizer(minimumValue, maximumValue, customSeed);
+
+    for (int i = 0; i < 1000; i++) {
+        auto randomValue(randomizer.getRandomValue());
+        ASSERT_GE(randomValue, minimumValue);
+        ASSERT_LE(randomValue, maximumValue);
+    }
+}
+
+TEST(AlbaUniformDeterministicRandomizerTest, GetRandomIntegerWorks_AsDeterministic) {
+    constexpr int minimumValue(0);
+    constexpr int maximumValue(9);
+    constexpr int customSeed(5);
+    IntegerRandomizerForTest randomizer1(minimumValue, maximumValue, customSeed);
+    IntegerRandomizerForTest randomizer2(minimumValue, maximumValue, customSeed);
+
+    for (int i = 0; i < 1000; i++) {
+        ASSERT_EQ(randomizer1.getRandomValue(), randomizer2.getRandomValue());
+    }
+}
+
+TEST(AlbaUniformDeterministicRandomizerTest, GetRandomIntegerWorks_AsUniformlyDistributed) {
+    constexpr int minimumValue(0);
+    constexpr int maximumValue(9);
+    constexpr int customSeed(5);
+    IntegerRandomizerForTest randomizer(minimumValue, maximumValue, customSeed);
+    int numberOfRandomValues(maximumValue - minimumValue + 1);
+    int const iterations(1000);
+    int const allowedDeviationCount(iterations / 10);
+    vector<int> hitsForEachValue(static_cast<unsigned long>(numberOfRandomValues), 0);
+
+    for (int i = 0; i < iterations; i++) {
+        auto randomValue(randomizer.getRandomValue());
+        hitsForEachValue[static_cast<unsigned long>(randomValue)]++;
+    }
+
+    ASSERT_FALSE(hitsForEachValue.empty());
+    auto&& [minIterator, maxIterator] = std::minmax_element(hitsForEachValue.cbegin(), hitsForEachValue.cend());
+    int deviationCount(*maxIterator - *minIterator);
+    EXPECT_LT(deviationCount, allowedDeviationCount);
+}
+
+TEST(AlbaUniformDeterministicRandomizerTest, GetRandomFloatingValueWorks_WithinMinimumAndMaximumValues) {
+    constexpr double minimumValue(-11.5);
+    constexpr double maximumValue(23.25);
+    constexpr double customSeed(10);
+    FloatingPointRandomizerForTest randomizer(minimumValue, maximumValue, customSeed);
+
+    for (int i = 0; i < 1000; i++) {
+        auto randomValue(randomizer.getRandomValue());
+        ASSERT_GE(randomValue, minimumValue);
+        ASSERT_LE(randomValue, maximumValue);
+    }
+}
+
+TEST(AlbaUniformDeterministicRandomizerTest, GetRandomFloatingValueWorks_AsDeterministic) {
+    constexpr double minimumValue(-11.5);
+    constexpr double maximumValue(23.25);
+    constexpr double customSeed(10);
+    FloatingPointRandomizerForTest randomizer1(minimumValue, maximumValue, customSeed);
+    FloatingPointRandomizerForTest randomizer2(minimumValue, maximumValue, customSeed);
+
+    for (int i = 0; i < 1000; i++) {
+        ASSERT_EQ(randomizer1.getRandomValue(), randomizer2.getRandomValue());
+    }
+}
+
+TEST(AlbaUniformDeterministicRandomizerTest, GetRandomFloatingValueWorks_AsUniformlyDistributed) {
+    constexpr int minimumValue(0);
+    constexpr int maximumValue(9);
+    constexpr double customSeed(10);
+    FloatingPointRandomizerForTest randomizer(
+        static_cast<double>(minimumValue), static_cast<double>(maximumValue), customSeed);
+    int numberOfRandomValues(maximumValue - minimumValue);  // number of integer values for floating point
+    int const iterations(1000);
+    int const allowedDeviationCount(iterations / 10);
+    vector<int> hitsForEachValue(numberOfRandomValues, 0);
+
+    for (int i = 0; i < iterations; i++) {
+        auto randomValue(randomizer.getRandomValue());
+        hitsForEachValue[static_cast<unsigned long>(randomValue)]++;
+    }
+
+    ASSERT_FALSE(hitsForEachValue.empty());
+    auto&& [minIterator, maxIterator] = std::minmax_element(hitsForEachValue.cbegin(), hitsForEachValue.cend());
+    int deviationCount(*maxIterator - *minIterator);
+    EXPECT_LT(deviationCount, allowedDeviationCount);
+}
+
+}  // namespace alba
