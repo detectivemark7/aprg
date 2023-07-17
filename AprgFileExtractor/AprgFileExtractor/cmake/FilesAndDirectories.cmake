b0dfffe83ab6ac95d0999e05b852184876bf450b
detectivemark7 <detectivemark7@gmail.com>
2023-07-18 01:52:13 +0800

    update

--- AprgFileExtractor/AprgFileExtractor/cmake/FilesAndDirectories.cmake
+++ AprgFileExtractor/AprgFileExtractor/cmake/FilesAndDirectories.cmake
@@ -1,6 +1,3 @@
-include(${APRG_DIR}/AprgCMakeHelpers/FileAndDirectoryMacros.cmake)
-include(${APRG_DIR}/AprgCMakeHelpers/PrintMacros.cmake)
-
 set(APRG_FILE_EXTRACTOR_SOURCE_DIRECTORY
     ${APRG_FILE_EXTRACTOR_DIR}/src
 )
--- AprgFileExtractor/AprgFileExtractor/src/FileExtractor/AprgFileExtractor.cpp
+++ AprgFileExtractor/AprgFileExtractor/src/FileExtractor/AprgFileExtractor.cpp
@@ -146,7 +146,7 @@ void AprgFileExtractor::extractAllRelevantFilesRecursively(string const& filePat
     set<string> filePaths;
     copyRelativeFilePathsFromCompressedFile(filePathOfCompressedFile, filePaths);
     ProgressCounters::numberOfFilesToBeAnalyzedForExtraction += filePaths.size();
-    for (string const filePath : filePaths) {
+    for (string const& filePath : filePaths) {
         AlbaLocalPathHandler filePathHandler(filePath);
         if (m_grepEvaluator.evaluate(filePathHandler.getFile())) {
             AlbaLocalPathHandler extractedPathHandler(extractOneFile(filePathOfCompressedFile, filePath));
