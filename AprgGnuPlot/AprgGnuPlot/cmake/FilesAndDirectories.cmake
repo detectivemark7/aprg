5642818336cf50495fbb5840a2ccbdb595a7773d
detectivemark7 <detectivemark7@gmail.com>
2023-07-18 19:43:27 +0800

    update

--- AprgGnuPlot/AprgGnuPlot/cmake/FilesAndDirectories.cmake
+++ AprgGnuPlot/AprgGnuPlot/cmake/FilesAndDirectories.cmake
@@ -1,6 +1,3 @@
-include(${APRG_DIR}/AprgCMakeHelpers/FileAndDirectoryMacros.cmake)
-include(${APRG_DIR}/AprgCMakeHelpers/PrintMacros.cmake)
-
 set(APRG_GNU_PLOT_SOURCE_DIRECTORY
     ${APRG_GNU_PLOT_DIR}/src
 )
