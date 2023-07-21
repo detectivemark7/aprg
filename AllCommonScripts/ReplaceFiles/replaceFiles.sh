3c9928a97397931021994cadc2e76cb1bfae8ca0
detectivemark7 <detectivemark7@gmail.com>
2023-07-22 05:48:33 +0800

    update

--- AllCommonScripts/ReplaceFiles/replaceFiles.sh
+++ AllCommonScripts/ReplaceFiles/replaceFiles.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Set the source file path and the target folder path
-sourceFilePath="/mnt/f/Branches/APRG_GIT/aprg/aprg/111_AprgScripts/ReplaceFiles/CMakePresets.json"
+# Set the variables
+# sourceFilePath="/mnt/f/Branches/APRG_GIT/aprg/aprg/111_AprgScripts/ReplaceFiles/CMakePresets.json"
+sourceFilePath=$1
 targetFolderPath="/mnt/f/Branches/APRG_GIT/aprg/aprg/"
+skipRegexPattern="\/aprg\/Python\/"
 
 # Get the file name from the source file path
 sourceFilename=$(basename "$sourceFilePath")
@@ -14,11 +16,11 @@ targetFilePaths=($(find "$targetFolderPath" -type f -name "$sourceFilename"))
 
 # Replace each file with the source file
 for targetFilePath in "${targetFilePaths[@]}"; do
-    # Check if its a different path
-	if [ "$sourceFilePath" != "$targetFilePath" ]; then
+    # Check if it can be skipped
+	if ! [[ "$targetFilePath" == $sourceFilePath || "$targetFilePath" =~ $skipRegexPattern ]]; then
 		shortenedSourcePath=$(echo "$sourceFilePath" | awk -F'/' '{print ".../"$(NF-2)"/"$(NF-1)"/"$(NF)}')
 		shortenedTargetPath=$(echo "$targetFilePath" | awk -F'/' '{print ".../"$(NF-2)"/"$(NF-1)"/"$(NF)}')
-        echo "Replacing $shortenedSourcePath with $shortenedTargetPath"
+        echo "Replacing [$shortenedSourcePath] with [$shortenedTargetPath]"
         cp -f "$sourceFilePath" "$targetFilePath"
 	fi
 done
