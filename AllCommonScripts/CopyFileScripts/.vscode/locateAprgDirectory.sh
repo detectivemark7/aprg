67a3622620edbc1ec414119d9c5a4555b4bcb5d6
detectivemark7 <detectivemark7@gmail.com>
2023-07-22 13:15:07 +0800

    update

---
+++ AllCommonScripts/CopyFileScripts/.vscode/locateAprgDirectory.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+findAprgDirectory() {
+	# Set variable values
+	aprgDirectory=""
+	local aprgDirectoryName="aprg"
+	local inputDirectory=$1
+	local localScriptName="locateAprgDirectory"
+	
+	# Validate input
+	if ! [ -d "$inputDirectory" ]; then
+		echo "$localScriptName:$LINENO The input directory [$inputDirectory] is not a directory"
+		return 1
+	fi
+	
+	# Display variable values
+	echo "$localScriptName:$LINENO The aprg directory name is [$aprgDirectoryName]."
+	echo "$localScriptName:$LINENO The input directory is [$inputDirectory] for searching [$aprgDirectoryName] directory."
+	
+	# Set variables for search
+	searchingDirectory="$inputDirectory"
+	
+	# Loop until we reach the root directory ("/")
+	while [ "$searchingDirectory" != "/" ]; do
+		# Check if the directory exists in the current directory
+		if [ -d "$searchingDirectory/$aprgDirectoryName" ]; then
+			aprgDirectory="$searchingDirectory/$aprgDirectoryName"
+			break
+		else
+			# Go one directory up
+			searchingDirectory=$(dirname "$searchingDirectory")
+		fi
+	done
+	
+	# Validate output
+	if [ "$searchingDirectory" == "/" ]; then
+		echo "$localScriptName:$LINENO The directory name [$aprgDirectoryName] is not found in any parent directories of [$inputDirectory]."
+		return 1
+	fi
+	
+	echo "$localScriptName:$LINENO The aprg directory is [$aprgDirectory]."
+}
--- AllCommonScripts/CopyFileScripts/.vscode/runWslCommonScript.sh
+++ AllCommonScripts/CopyFileScripts/.vscode/runWslCommonScript.sh
@@ -1,42 +1,18 @@
 #!/bin/bash
 
 # Set variable values
-aprgDirectoryName="aprg"
 projectDirectory=$(pwd)
+scriptDirectory=$(dirname "$0")
 scriptName=$(basename "$0")
 scriptRunningOption=$1
 
-# Display variable values
-echo "[Script: '$scriptName']: The aprg directory name is [$aprgDirectoryName] and the project directory is [$projectDirectory]."
-
-# Function to find the aprg directory
-findAprgAsAParentDirectory() {
-    local dir="$projectDirectory"
-
-    # Loop until we reach the root directory ("/")
-    while [ "$dir" != "/" ]; do
-        # Check if the directory exists in the current directory
-        if [ -d "$dir/$aprgDirectoryName" ]; then
-			aprgDirectory="$dir/$aprgDirectoryName"
-            return
-        fi
-
-        # Go one directory up
-        dir=$(dirname "$dir")
-    done
-
-    echo "[Script: '$scriptName']: The directory name [$aprgDirectoryName] is not found in any parent directories."
-}
-
-# Locating aprg directory
-echo "[Script: '$scriptName']: Locating aprg directory..."
-aprgDirectory=""
-findAprgAsAParentDirectory
-echo "[Script: '$scriptName']: The aprg directory is [$aprgDirectory]"
-
+# Source needed scripts
+source "$scriptDirectory/locateAprgDirectory.sh" 
+findAprgDirectory $projectDirectory
+echo "aprgDirectory"
+source "$aprgDirectory/AllCommonScripts/PrintScripts/PrintUtilities.sh"
 
 # Running WSL Build And Run Script
-
 if [ "$scriptRunningOption" == "outputWithHighlighting" ]; then
 	# This command prints output with highlighting because stdout and stderr are maintained
 	bash "$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 $4
@@ -47,7 +23,7 @@ elif [ "$scriptRunningOption" == "outputWithRelativePaths" ]; then
     # This command prints output without highlighting but with corrected relative paths from the project directory
 	bash "$aprgDirectory/AllCommonScripts/WslScripts/BuildAndRunInWsl.sh" $2 $3 $4 2>&1 | sed -E "s|$projectDirectory||g"
 else
-	echo "[Script: '$scriptName']: The script option [$scriptOption] is not found."
+	scriptPrint $scriptName $LINENO "The script option [$scriptOption] is not found."
 	echo "$scriptPath:$LINENO:${BASH_LINENO[0]}: error: The script option [$scriptOption] is not supported by the WSL shell script."
 	exit 1
 fi
